140afb4cb0a08c2b9de430ce453ffff06fced965
jodavis <jdavis@shoedazzle.com>
2019-01-21 21:22:16 -0800

    Jamming - stuck on steps 85-87

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
+import Spotify from '../../util/Spotify';
 // import logo from './logo.svg';
 import './App.css';
 
@@ -53,6 +54,9 @@ class App extends Component {
     };
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
   }
   addTrack(track) {
     console.log('addTrack running');
@@ -73,6 +77,18 @@ class App extends Component {
     console.log('newPlaylistTracks: ');
     console.log(newPlaylistTracks);
   }
+  updatePlaylistName(name) {
+    this.setState({playlistName: name});
+  }
+  savePlaylist(){
+    const uriList = this.state.playlistTracks.map(track => track.uri);
+    console.log('uriList: ');
+    console.log(uriList);
+  }
+  search(term){
+    console.log('term: ' + term);
+    Spotify.search(term);
+  }
 
   render() {
     console.log('App this.state:', this.state);
@@ -80,10 +96,10 @@ class App extends Component {
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
-          <SearchBar />
+          <SearchBar onSearch={this.search} />
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
-            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} />
+            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName} />
           </div>
         </div>
       </div>
--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -4,13 +4,21 @@ import TrackList from '../TrackList/TrackList'
 
 
 class Playlist extends React.Component {
+	constructor(props) {
+		super(props);
+		this.handleNameChange = this.handleNameChange.bind(this);
+	}
+	handleNameChange(e){
+		const newName = e.target.value;
+		this.props.onNameChange(newName);
+	}
 	render() {
 		console.log('Playlist this.props: ', this.props);
 		return (
 			<div className="Playlist">
-				<input defaultValue={'New Playlist'} />
+				<input defaultValue={this.props.playlistName} onChange={this.handleNameChange}/>
 				<TrackList tracks={this.props.playlistTracks} onRemove={this.props.onRemove} isRemoval={true} />
-  				<a className="Playlist-save">SAVE TO SPOTIFY</a>
+				<a className="Playlist-save" onClick={this.props.onSave}>SAVE TO SPOTIFY</a>
 			</div>
 		)
 	}
